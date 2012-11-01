86bcd8fa6a2806fe8927f314486b6033cc4f7b6a
Johan Kock <johan.kock@creuna.se>
2012-11-01 16:21:53 +0100

    awesome searhplaylist

--- proteam5/js/scripts.js
+++ proteam5/js/scripts.js
@@ -1,25 +1,37 @@
-
+window.onload = function() {
 var sp = getSpotifyApi(1), // Getthe spotify api
     models = sp.require('sp://import/scripts/api/models'); // Get models
+ var views = sp.require('sp://import/scripts/api/views');
+var search = new models.Search('Rain');
+
+var player = models.player;
 
 // Listen to the eventlisteners indicating change of tab
-models.application.observe(models.EVENT.ARGUMENTSCHANGED, function (e) {
-  var b = document.getElementById('head'), // Get the 'bodu' element (<head>)
-      arg = models.application.arguments[0]; // Get the values of the tabs
+search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+search.searchAlbums = false;
+var multiple_tracks_playlist = new models.Playlist();
 
-  // Change class on the head-element
-  switch (arg) {
-    case 'Blue':
-      b.setAttribute('class', 'bg1');
-      break;
-    case 'Red':
-      b.setAttribute('class', 'bg2');
-      break;
-    case 'Green':
-      b.setAttribute('class', 'bg3');
-      break;
-    default:
-      console.log('error:: undefined tab');
-      break;
-    }
+    //for(var i=0;i<20;i++) {
+        //var library_track = models.Track.fromURI(library_tracks[i].data.uri);
+        //multiple_tracks_playlist.add(library_track);
+    //}
+search.observe(models.EVENT.CHANGE, function() {
+  search.tracks.forEach(function(track) {
+    console.log(track);
+    multiple_tracks_playlist.add(track.uri);
+  });
 });
+
+console.log(multiple_tracks_playlist);
+
+var multiple_tracks_player = new views.List(multiple_tracks_playlist);
+    multiple_tracks_player.track = null; // Don't play the track right away
+    multiple_tracks_player.context = multiple_tracks_playlist;
+   
+    /* Pass the player HTML code to #multiple-tracks-player */
+    var multiple_tracks_player_HTML = document.getElementById('multiple-tracks-player');
+    console.log(multiple_tracks_player_HTML);
+    multiple_tracks_player_HTML.appendChild(multiple_tracks_player.node);
+
+search.appendNext();
+}
