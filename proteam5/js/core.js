cde3ca887c2d8d5064cc63e0ecc6c7fcc77a0c97
Johan Kock <johan.kock@creuna.se>
2012-11-01 17:38:35 +0100

    more awesomeness

--- proteam5/js/core.js
+++ proteam5/js/core.js
@@ -7,6 +7,7 @@ var sp = getSpotifyApi(1),
 
 view = {
   init : function() {
+
       $.ajax({
         url: 'http://api.yr.no/weatherapi/locationforecast/1.8/?lat=59.33;lon=18.07',
         dataType: 'xml',
@@ -15,19 +16,14 @@ view = {
       });
 
 
-
       // Listen to the eventlisteners indicating change of tab
       search.localResults = models.LOCALSEARCHRESULTS.APPEND;
       search.searchAlbums = false;
       var multiple_tracks_playlist = new models.Playlist();
 
-          //for(var i=0;i<20;i++) {
-              //var library_track = models.Track.fromURI(library_tracks[i].data.uri);
-              //multiple_tracks_playlist.add(library_track);
-          //}
       search.observe(models.EVENT.CHANGE, function() {
         search.tracks.forEach(function(track) {
-          console.log(track);
+//          console.log(track);
           multiple_tracks_playlist.add(track.uri);
         });
       });
@@ -35,14 +31,20 @@ view = {
       console.log(multiple_tracks_playlist);
 
       var multiple_tracks_player = new views.List(multiple_tracks_playlist);
-          multiple_tracks_player.track = null; // Don't play the track right away
-          multiple_tracks_player.context = multiple_tracks_playlist;
-         
-          /* Pass the player HTML code to #multiple-tracks-player */
-          var multiple_tracks_player_HTML = document.getElementById('multiple-tracks-player');
-          console.log(multiple_tracks_player_HTML);
-          multiple_tracks_player_HTML.appendChild(multiple_tracks_player.node);
+//      multiple_tracks_player.track = null; // Don't play the track right away
+      multiple_tracks_player.context = multiple_tracks_playlist;
 
+      console.log(multiple_tracks_playlist.data.uri);
+
+      models.player.play(multiple_tracks_playlist.data.uri);
+
+      var $playlist = $('#play-list'),
+          play_list_href = '<a href="' + multiple_tracks_playlist.uri + ' ">Play</a>';
+
+      $playlist.append(multiple_tracks_player.node);
+//      $playlist.prepend(play_list_href);
+
+      //important dont remove!!
       search.appendNext();
   },  
 
